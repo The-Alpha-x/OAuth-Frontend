002eb32a0cd67b07fb437bc52e8267813f7c5e08
JayaniAmanda <amandajayani1998@gmail.com>
2021-10-03 22:08:22 +0530

    last commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import FileList from "./Components/FileList";
 import FileUpload from "./Components/FileUpload";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" component={Home} exact/>
           <Route path="/list" component={FileList} exact/>
           <Route path="/upload" component={FileUpload} exact/>
+          <Route path="/menu" component={Menu} exact/>
         </Switch>
       </Router>
     </div>
